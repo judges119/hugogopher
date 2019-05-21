c2ea3bfe238702841fa89ec7a26d505cc1db4ec8
Adam O'Grady <adam.ogrady@gmail.com>
2019-05-21 21:00:30 +0800

    removed dead code, prefixed post names with the publish date

--- gophermap.js
+++ gophermap.js
@@ -2,12 +2,6 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = function gophermap(gophermap_listing, files_array, directory, post_dir, post) {
-  const date = post.metadata.date;
-  const year = date.slice(0, 4);
-  const month = date.slice(5, 7);
-  const day = date.slice(8, 10);
-  const final_date = new Date(parseInt(year, 10), parseInt(month, 10) - 1, parseInt(day, 10), 8);
-
   const gophermap_listing_longer = gophermap_listing.concat({
     location: post_dir + path.sep + post.metadata.slug,
     title: post.metadata.title,
@@ -24,7 +18,7 @@ module.exports = function gophermap(gophermap_listing, files_array, directory, p
       return 0;
     })
     fs.promises.writeFile('gopher' + path.sep + directory + path.sep + 'gophermap', gophermap_listing_longer_sorted.reduce((file, listing) => {
-      return file + '0' + listing.title + '	/' + listing.location + '\n';
+      return file + '0' + listing.date + ' ' +  listing.title + '	/' + listing.location + '\n';
     }, ''));
   }
   return gophermap_listing_longer;
